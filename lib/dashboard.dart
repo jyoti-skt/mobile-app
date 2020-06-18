c84b88a75b85bb66481dd966ec19f3b33b6aba87
Admin <gogamein@gmail.com>
2020-06-18 16:34:52 +0530

    dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -9,6 +9,44 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+
+        body: Center(
+        child: Container(
+        color: Colors.white,
+        child: Padding(
+        padding: const EdgeInsets.all(30.0),
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+    Padding(
+    padding: const EdgeInsets.fromLTRB(12,1, 12, 6),
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: <Widget>[
+    Padding(
+    padding: const EdgeInsets.fromLTRB(5, 1, 12,6),
+    child: Image.asset("assets/logo.png",height: 40,width: 50),
+    ),
+    ],
+    ),
+    ),
+    Column(
+    children: <Widget>[
+    Text("Play | Socialise | Earn",style: TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Inter-Black-slnt'),),
+    ],
+    ),
+        ],
+    ),
+        ),
+    ),
+    ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:Sample/signup.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dashboard.dart';
 import 'login.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: LoginPage (),
+      home:  DashboardPage (),
     )
   );
 
